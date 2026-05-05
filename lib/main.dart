1dbe538941adc24e9be6f7413d205eef23065abf
Al-Zng <mohammedznger@gmail.com>
2026-05-05 21:36:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_state.dart';
-import 'screen/home_screen.dart';      // ✅ عدل من screens إلى screen
+import 'screen/home_screen.dart';
 import 'theme.dart';
 
 void main() {
