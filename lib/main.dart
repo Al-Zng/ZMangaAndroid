0a29be1e1ed1400e16e5725711a644b42dd5b121
Al-Zng <mohammedznger@gmail.com>
2026-05-05 20:51:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'app_state.dart';
+import 'screens/home_screen.dart';
+import 'theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState(),
+      child: const ZMangaApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ZMangaApp extends StatelessWidget {
+  const ZMangaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color(0xFF000000),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+      theme: ZTheme.darkTheme,
+      home: const HomeScreen(),
     );
   }
 }
