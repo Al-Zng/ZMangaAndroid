7ef802baf87b4e3989cfd4fae4f89c80787f7372
Al-Zng <mohammedznger@gmail.com>
2026-05-06 01:26:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_state.dart';
 import 'screen/main_shell.dart';
-import 'screen/cloudflare_sheet.dart';   // ✅ import مهم
 import 'theme.dart';
 
 void main() {
@@ -14,48 +13,8 @@ void main() {
   );
 }
 
-class ZMangaApp extends StatefulWidget {
+class ZMangaApp extends StatelessWidget {
   const ZMangaApp({super.key});
-
-  @override
-  State<ZMangaApp> createState() => _ZMangaAppState();
-}
-
-class _ZMangaAppState extends State<ZMangaApp> {
-  bool _cloudflareOpen = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final state = context.read<AppState>();
-      state.addListener(_checkCloudflare);
-    });
-  }
-
-  @override
-  void dispose() {
-    context.read<AppState>().removeListener(_checkCloudflare);
-    super.dispose();
-  }
-
-  void _checkCloudflare() {
-    final state = context.read<AppState>();
-    if (!state.showCloudflareSheet || _cloudflareOpen) return;
-    _cloudflareOpen = true;
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (_) => CloudflareSheet(),   // ✅ إزالة const
-          settings: const RouteSettings(name: '/cloudflare'),
-        ),
-      ).then((_) {
-        _cloudflareOpen = false;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
