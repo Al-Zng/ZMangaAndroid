e96d4fc6079f871811bd0df41d200e1303a87252
Al-Zng <mohammedznger@gmail.com>
2026-05-08 18:57:58 +0300

    Update http_service.dart

--- lib/core/network/http_service.dart
+++ lib/core/network/http_service.dart
@@ -1,44 +1,53 @@
 import 'package:dio/dio.dart';
+
 import '../cloudflare/cookie_service.dart';
 import 'user_agent.dart';
 
 class HttpService {
   static final HttpService _instance = HttpService._internal();
+
   factory HttpService() => _instance;
+
   HttpService._internal();
 
-  final Dio _dio = Dio(BaseOptions(
-    connectTimeout: const Duration(seconds: 15),
-    receiveTimeout: const Duration(seconds: 15),
-    headers: {
-      'User-Agent': AppUserAgent.iosSafari,
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language': 'ar,en;q=0.9',
-    },
-  ));
+  final Dio dio = Dio();
 
-  Dio get dio {
-    _dio.interceptors.clear();
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        final cookieService = CookieService();
-        final cookieHeader = await cookieService.getCookieHeader();
-        if (cookieHeader.isNotEmpty) {
-          options.headers['Cookie'] = cookieHeader;
-        }
-        options.headers['Referer'] = 'https://lekmanga.site/';
-        options.headers['Origin'] = 'https://lekmanga.site';
-        return handler.next(options);
+  final CookieService _cookieService = CookieService();
+
+  Future<void> initialize() async {
+    dio.options = BaseOptions(
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      sendTimeout: const Duration(seconds: 30),
+      followRedirects: true,
+      validateStatus: (status) => status != null,
+      headers: {
+        'User-Agent': AppUserAgent.iosSafari,
+        'Accept':
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
       },
-    ));
-    return _dio;
-  }
+    );
 
-  Future<Response> get(String url, {Map<String, dynamic>? queryParameters, Options? options}) async {
-    return dio.get(url, queryParameters: queryParameters, options: options);
-  }
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final cookieHeader =
+              await _cookieService.getCookieHeader();
 
-  Future<Response> post(String url, {dynamic data, Options? options}) async {
-    return dio.post(url, data: data, options: options);
+          if (cookieHeader.isNotEmpty) {
+            options.headers['Cookie'] = cookieHeader;
+          }
+
+          options.headers['User-Agent'] =
+              AppUserAgent.iosSafari;
+
+          handler.next(options);
+        },
+      ),
+    );
   }
 }
